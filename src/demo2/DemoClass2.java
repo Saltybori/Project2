d9a0d9674862e0909bacd9dd0d9efced56a6e6c9
ian_c <ian_c@Thetool>
2023-04-05 22:26:35 -0400

    Innitial Commit

---
+++ src/demo2/DemoClass2.java
@@ -0,0 +1,12 @@
+package demo2;
+
+public class DemoClass2 {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+		
+		System.out.println("Hello");
+		System.out.println("GitHub Lecture");
+	}	
+
+}
